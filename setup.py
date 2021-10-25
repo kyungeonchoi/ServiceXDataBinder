fc2f0fafa44e57bc02bfe75b6e6b190e78df780f
KyungEon Choi <kyungeonchoi@utexas.edu>
2021-10-25 21:18:06 +0900

    Add requirement for func_adl_servicex package

--- setup.py
+++ setup.py
@@ -36,7 +36,8 @@ setuptools.setup(name="servicex_databinder",
                     "nest-asyncio>=1.5.1",                
                     "tqdm>=4.60.0",
                     "pyarrow>=3.0.0",
-                    "backoff>=1.11.1"
+                    "backoff>=1.11.1",
+                    "func_adl_servicex>=1.1"
                     ],
                  classifiers=[
                     "Development Status :: 3 - Alpha",
