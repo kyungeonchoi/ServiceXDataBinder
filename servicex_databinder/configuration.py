9f3266cd4f000e0ed012859d950a33cd3dc1896c
KyungEon Choi <kyungeon@gmail.com>
2023-04-30 23:29:42 -0500

    clean up

--- servicex_databinder/configuration.py
+++ servicex_databinder/configuration.py
@@ -4,14 +4,18 @@ from typing import Any, Dict, Union
 import logging
 log = logging.getLogger(__name__)
 
-def _load_config(input_config: Union[str, pathlib.Path, Dict[str, Any]]) -> Dict[str, Any]:
+
+def _load_config(input_config:
+                 Union[str, pathlib.Path, Dict[str, Any]]
+                 ) -> Dict[str, Any]:
     """Loads, validates, and returns a config file from the provided path.
     Args:
-        input_config (Union[str, pathlib.Path, Dict[str, Any]]): path to config file or config as dict 
+        input_config (Union[str, pathlib.Path, Dict[str, Any]]):
+            path to config file or config as dict
     Returns:
         Dict[str, Any]: configuration
     """
-    
+
     if isinstance(input_config, dict):
         _replace_definition_in_sample_block(input_config)
         _validate_config(input_config)
@@ -24,8 +28,10 @@ def _load_config(input_config: Union[str, pathlib.Path, Dict[str, Any]]) -> Dict
             _replace_definition_in_sample_block(config)
             _validate_config(config)
             return config
-        except:
-            raise FileNotFoundError(f"Exception occured while reading config file: {file_path}")
+        except Exception:
+            raise FileNotFoundError(
+                f"Exception occured while reading config file: {file_path}"
+                )
 
 
 def _replace_definition_in_sample_block(config: Dict[str, Any]):
@@ -36,8 +42,12 @@ def _replace_definition_in_sample_block(config: Dict[str, Any]):
                 if 'DEF_' in value:
                     for repre, new_str in config.get('Definition').items():
                         if repre in value:
-                            log.debug(f"Replace Definition for {sample['Name']} - {field}: {repre} with {new_str}")
-                            config.get('Sample')[n][field] = config.get('Sample')[n][field].replace(repre, new_str)
+                            log.debug(
+                                f"Replace Definition for {sample['Name']} "
+                                f"- {field}: {repre} with {new_str}")
+                            config.get('Sample')[n][field] \
+                                = config.get('Sample')[n][field] \
+                                        .replace(repre, new_str)
                             flag = True
         return flag
     else:
@@ -45,8 +55,10 @@ def _replace_definition_in_sample_block(config: Dict[str, Any]):
 
 
 def _validate_config(config: Dict[str, Any]) -> bool:
-    """Returns True if the config file is validated, otherwise raises exceptions.
-    Checks that the config satisfies the json schema, and performs additional checks to
+    """Returns True if the config file is validated,
+    otherwise raises exceptions.
+    Checks that the config satisfies the json schema,
+    and performs additional checks to
     validate the config further.
     Args:
         config (Dict[str, Any]): configuration
@@ -57,38 +69,58 @@ def _validate_config(config: Dict[str, Any]) -> bool:
     Returns:
         bool: whether the validation was successful
     """
-    
+
     if 'General' not in config.keys() and 'Sample' not in config.keys():
-        raise KeyError(f"You should have 'General' and 'Sample' in the config")
-    
+        raise KeyError("You should have 'General' and 'Sample' in the config")
+
     if 'ServiceXBackendName' not in config['General'].keys():
-        raise KeyError(f"ServiceXBackendName is required")
+        raise KeyError("ServiceXBackendName is required")
     elif 'uproot' not in config['General']['ServiceXBackendName'].lower() and \
          'xaod' not in config['General']['ServiceXBackendName'].lower():
-        raise ValueError(f"ServiceXBackendName should contain either uproot or xaod")
+        raise ValueError(
+            "ServiceXBackendName should contain either uproot or xaod"
+            )
 
     if 'OutputDirectory' not in config['General'].keys():
-        raise KeyError(f"OutputDirectory is required")
+        raise KeyError("OutputDirectory is required")
 
     if 'OutputFormat' not in config['General'].keys():
-        raise KeyError(f"OutputFormat is required")
+        raise KeyError("OutputFormat is required")
     elif config['General']['OutputFormat'].lower() != 'parquet' and \
-         config['General']['OutputFormat'].lower() != 'root':
-        raise ValueError(f"OutputFormat can be either parquet or root")
+            config['General']['OutputFormat'].lower() != 'root':
+        raise ValueError("OutputFormat can be either parquet or root")
 
-    for sample in config['Sample']:        
-        if 'RucioDID' not in sample.keys() and 'XRootDFiles' not in sample.keys() and 'LocalPath' not in sample.keys():
-            raise KeyError(f"Please specify a valid input option for Sample {sample['Name']} e.g. RucioDID")
+    for sample in config['Sample']:
+        if ('RucioDID' not in sample.keys()) \
+                and ('XRootDFiles' not in sample.keys()) \
+                and ('LocalPath' not in sample.keys()):
+            raise KeyError(
+                "Please specify a valid input option "
+                f"for Sample {sample['Name']} e.g. RucioDID"
+                )
         if 'RucioDID' in sample.keys():
             for did in sample['RucioDID'].split(","):
                 if len(did.split(":")) != 2:
-                    raise ValueError(f"Sample {sample['Name']} - RucioDID {did} is missing the scope")
-        if 'Tree' in sample and 'uproot' not in config['General']['ServiceXBackendName'].lower():
-            raise KeyError(f"Tree in Sample {sample['Name']} is only for uproot backend type")
+                    raise ValueError(
+                        f"Sample {sample['Name']} "
+                        f"- RucioDID {did} is missing the scope"
+                        )
+        if ('Tree' in sample) and \
+                ('uproot' not in config['General']['ServiceXBackendName'].lower()):
+            raise KeyError(
+                f"Tree in Sample {sample['Name']} "
+                "is only for uproot backend type"
+                )
         if 'Columns' in sample and 'FuncADL' in sample:
-            raise KeyError(f"Sample {sample['Name']} - Use one type of query per sample: Columns for TCut and FuncADL for func-adl")
+            raise KeyError(
+                f"Sample {sample['Name']} - Use one type of query per sample: "
+                "Columns for TCut and FuncADL for func-adl"
+                )
         if 'FuncADL' in sample and 'Filter' in sample:
-            raise KeyError(f"Sample {sample['Name']} - You cannot use Filter with func-adl query")
+            raise KeyError(
+                f"Sample {sample['Name']} - "
+                "You cannot use Filter with func-adl query"
+                )
 
     log.debug("config looks okay")
     return True
